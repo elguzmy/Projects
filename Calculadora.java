37a95b7d5dc659e9e47d9583a775b88a03c91fc1
Guzman Andrade <guzmy@MacBook-Pro-de-Guzman.local>
2017-11-08 09:47:39 -0300

    Soporto Resta

--- Calculadora.java
+++ Calculadora.java
@@ -2,11 +2,17 @@
 public class Calculadora {
 
 public static void main (String [] args){
-	if(args.length==3){
-	try{
-		int op1= Integer.parseInt(args[1]);
-		int op2= Integer.parseInt(args[2]);
-		System.out.println(op1 + op2);
+if(args.length==3){
+try{
+int op1= Integer.parseInt(args[1]);
+int op2= Integer.parseInt(args[2]);
+if("+".equals(args[0])){
+System.out.println(op1+op2);
+}else if("-".equals(args[0])){
+   System.out.println(op1-op2);
+}else{
+System.out.println(args[0] + ": operacion desconocida");	
+}
 
 }catch(Exception ex){
 System.out.println("Parametros incorrectos");
